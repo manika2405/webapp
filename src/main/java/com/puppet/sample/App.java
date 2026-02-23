4eeadafd3479ea624998fc42a90cad46d394f58a
manika2405 <manikasingla2005@gmail.com>
2026-02-23 20:40:21 +0530

    change

--- src/main/java/com/puppet/sample/App.java
+++ src/main/java/com/puppet/sample/App.java
@@ -16,7 +16,7 @@ public class App
 
   public String enMsg()
   {
-    return "Hello World!";
+    return "Hello World from Manika!";
   }
 
   private static String requestInfoToString(Request request) {
